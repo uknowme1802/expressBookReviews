d24e6b25b7e894e8d0bc1fe65ddb91da3eed5d03
uknowme1802 <harshdeepsharma654@gmail.com>
2025-07-14 21:45:55 -0400

    book review2

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -12,6 +12,15 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+        let validusers = users.filter((user) => {
+            return (user.username === username && user.password === password);
+        });
+        // Return true if any valid user is found, otherwise false
+        if (validusers.length > 0) {
+            return true;
+        } else {
+            return false;
+        }
 }
 
 //only registered users can login
@@ -31,7 +40,15 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.authorization.username;
+    if (books[isbn]) {
+        books[isbn].reviews[username] = review;
+        return res.status(200).json({ message: "Review updated", reviews: books[isbn].reviews });
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
 });
 
 module.exports.authenticated = regd_users;
